cc743eb637af014d355be5f972ae350df7860dbe
Sabrina Settle <ssettle@e1z2r8p20.42.us.org>
2019-04-14 18:01:13 -0700

    more

--- Girdle08/fromherculeswithlove.py
+++ Girdle08/fromherculeswithlove.py
@@ -6,18 +6,16 @@
 #    By: ssettle <ssettle@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/03/20 12:44:32 by ssettle           #+#    #+#              #
-#    Updated: 2019/04/11 18:20:38 by ssettle          ###   ########.fr        #
+#    Updated: 2019/04/11 18:31:07 by ssettle          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #Mandorty part: Send an email out to warriors to join your army! Write a program that uses SMTP to send email
 
-
-
 import smtplib
 import os
 import sys
-import time`
+import time
 
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEtext
