177abcf83b05252b1c8f69eeb7d0fe85f46a8f12
Sabrina Settle <ssettle@e1z2r8p20.42.us.org>
2019-04-18 15:52:28 -0700

    Cleaned Bull

--- 06Bull/sumobabyboss.s
+++ 06Bull/sumobabyboss.s
@@ -1,18 +1,17 @@
-.name "sumobabyboss'
+.name "sumobabyboss"
 .comment "*headbutts in sign langauge*"
 
-fork:
-	fork %:livespawn ;
+entry:
+	fork %:spawn ; [0x0c] create a new process that will inherit
 
-hello:
+l2:
 	sti r1, %:live, %1
 	and r1, %0, r1
 
 live:
-	live %1 ; [0x01] allows a process to stay alive.
+	live %1
 	zjmp %:live
 
 spawn:
-	live %1
+	live %1	; [0x01] allows a process to stay alive.
 
-//is at the same place as a zork whatever that means for the most part
