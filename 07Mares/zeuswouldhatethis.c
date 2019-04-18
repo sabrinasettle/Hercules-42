d3114437ec95f8c5cf42bdfa66a85e16cf2bdcbc
Sabrina Settle <ssettle@e1z2r8p20.42.us.org>
2019-04-18 16:58:07 -0700

    Added vm user

--- 07Mares/zeuswouldhatethis.c
+++ 07Mares/zeuswouldhatethis.c
@@ -6,13 +6,29 @@
 /*   By: ssettle <ssettle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/14 17:47:26 by ssettle           #+#    #+#             */
-/*   Updated: 2019/04/18 16:07:15 by ssettle          ###   ########.fr       */
+/*   Updated: 2019/04/18 16:54:54 by ssettle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// Marebreeder Sabrina123
 
+/*
+** Fork - fork() system herccall use for creates a new process, which is called a
+** child process
+*/
+
+
+/*
+** Signals in C - a signal is a software generated interrrupt that is sent to a
+** process by the OS because of when user presses ctrl-c or another process tell
+** something to this process. Any command like thing the the OS sends to the program
+** saying that somehting happened
+*/
+
+//SIGINT -- ctrl c
 
 int main(int argc, char **argc)//could be void
 {
 
-} 
+
+}
