cfc02ae0217f7f59873c9a44c9401e6ded8a9456
Sabrina Settle <ssettle@e1z2r8p20.42.us.org>
2019-04-17 21:18:17 -0700

    love herc

--- Girdle08/fromherculeswithlove.py
+++ Girdle08/fromherculeswithlove.py
@@ -6,22 +6,45 @@
 #    By: ssettle <ssettle@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/03/20 12:44:32 by ssettle           #+#    #+#              #
-#    Updated: 2019/04/11 18:31:07 by ssettle          ###   ########.fr        #
+#    Updated: 2019/04/17 21:16:22 by ssettle          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #Mandorty part: Send an email out to warriors to join your army! Write a program that uses SMTP to send email
 
 import smtplib
+import ssl #needed?
 import os
 import sys
 import time
+import getpass
 
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEtext
 
-sender =
-recipient =
+sender = input("With love from : (enter Herc)")
+senderpasswd = getpass.getpass("Herc's email password: ")
+recipient = input('hey whats your intra id?: (seriously enter your intra)')
+
+
+subject = 'WYD'
+body = 'But seriously gurl, nice girdle'
+bye = '<3 Herc'
+
+if sender == "Herc"
+	sender = "ssettle93@gmail.com" #maybe change for later??
+if '@' not in recipient:
+	recipient = recipient + '@student.42.us.org'
+
+try:
+	smtp = stmplib.SMTP('smtp.gmail.com', 587)
+	smtp.starttls()
+	smtp.login(sender, senderpasswd);
+	smtp.sendmail(sender, recipient, message)
+	smtp.quit()
+	print ("Message sent")
+except:
+	print ("OH DEAR NO GIRDLE FOR YOU HERC <3")
 
 
 
