7da35825c926b1a9f8dacf1ef4a163c4361221ab
Sabrina Settle <ssettle@e1z2r8p20.42.us.org>
2019-04-22 23:53:01 -0700

    mares done thanks to maya

--- 07Mares/mangermanager.sh
+++ 07Mares/mangermanager.sh
@@ -6,7 +6,7 @@
 #    By: ssettle <ssettle@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/04/18 19:51:50 by ssettle           #+#    #+#              #
-#    Updated: 2019/04/22 19:55:34 by ssettle          ###   ########.fr        #
+#    Updated: 2019/04/22 23:34:41 by ssettle          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -30,33 +30,33 @@ kill -s KILL $(pgrep manger ruins lampon xanthos crap) #didnt kill ruins totally
 # kills yes by itself by
 pkill yes
 # kills the manger
-kill -s KILL $(top -l 1| grep manger | cut -d " " -f 1)
+kill -s KILL $(top -l 1 | grep manger | cut -d " " -f 1)
 #removed the .poo files
 rm -rf *.poo
 
 # Continue to my manger
 clear; echo $'Hercules vs Mares won by Hercules\n'
 read -rsp $'Press Enter for round two\n'
+for ((i=3; i>0; i--))
+do 
+    clear; echo "In $i..."; sleep 1
+done
+clear;
 #compile .c here
-gcc ./zeuswouldhatethis.c -o zeuskillme
+gcc ./zeusisgonnahatethis.c -o zeuskillme
 #run my binary in background
 ./zeuskillme &
-# waits 10 seconds
-#sleep 2
+# waits 2 seconds
+sleep 2
 
-# # # Now we stop my creation
-# kill -s KILL $(pgrep osascript) # say
-#kill -s KILL $(pgrep zeuskillme) #
-# rm -rf *.shit
-
+# Now we stop my creation
 killall zeuskillme
-
-# rm zeuskillme
+echo "SUCK IT ZEUS!!!! :P"
+rm zeuskillme
 # Binary files contain bytes that are low level language interpretation
 
 # 9 and 17 SIGS
 # 9 : KILL
 # 17 : CHLD
-
 
 # call ps and top to moniter the processes
