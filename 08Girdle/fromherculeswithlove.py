1f9b56d56dd65e6451d254cefc59f7033916f994
Sabrina Settle <ssettle@e1z2r8p20.42.us.org>
2019-04-24 15:16:06 -0700

    Girdle up and working

--- 08Girdle/fromherculeswithlove.py
+++ 08Girdle/fromherculeswithlove.py
@@ -6,43 +6,79 @@
 #    By: ssettle <ssettle@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/03/20 12:44:32 by ssettle           #+#    #+#              #
-#    Updated: 2019/04/17 22:16:17 by ssettle          ###   ########.fr        #
+#    Updated: 2019/04/24 15:14:07 by ssettle          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-#Mandorty part: Send an email out to warriors to join your army! Write a program that uses SMTP to send email
-#apt-get shh for debian machine
+#!/usr/bin/env python
 
-import smtplib
-import ssl #needed?
 import os
-import sys
+import ssl
 import time
 import getpass
+import smtplib
 
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEtext
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
-sender = input("With love from : (enter Herc)")
-senderpasswd = getpass.getpass("Herc's email password: ")
-recipient = input('hey whats your intra id?: (seriously enter your intra)')
+def baby_tell_me_what_you_want():
+	sender = raw_input('With love from : (enter Herc) ') # gets the email address of Herc
+	if sender == "Herc":
+		sender = "hercloveshippo08@gmail.com"
+		senderpasswd = getpass.getpass("Herc's email password: ") # uses the getpass to get the password
+		#if senderpasswd !=
+		recipient = raw_input('hey whats your intra id?: (seriously enter your intra) ')
+		if '@' not in recipient: #adds the address to the intra login
+			recipient = recipient + '@student.42.us.org'
+	elif sender != "Herc": # catch for input
+		print ("This won't work if you dont work WITH me")
 
+	msg = MIMEMultipart()
+	msg['To'] = recipient
+	msg['From'] = sender
+	msg['Subject'] = 'WYD'
 
-subject = 'WYD'
-body = 'But seriously gurl, nice girdle'
-bye = '<3 Herc'
+	html = """\
+	<html>
+		<body>
+			<p>hey<br>
+			hey<br>
+			hey<br>
+			nice girdle<br>
+			<3 Herc
+			</p>
+		</body>
+	</html>
+	"""
+	body = MIMEText(html, 'html')
+	msg.attach(body)
 
-if sender == "Herc"
-	sender = "ssettle93@gmail.com" #maybe change for later??
-if '@' not in recipient:
-	recipient = recipient + '@student.42.us.org'
+	print('Connecting to the SMTP') # gets to this but doesnt send the email
+	# context = ssl.create_default_context()
+	try:
+		# Gmail SMTP
+		smtp = smtplib.SMTP('smtp.gmail.com', 587)
+		# Begin verfication of the connection to the SMTP gmail client
+		smtp.ehlo()
+		# Securing with TLS
+		smtp.starttls()
+		smtp.ehlo()
+		print ('Logging in...')
+		smtp.login(sender, senderpasswd)
+		thiccbody = msg.as_string()
+		smtp.sendmail(sender, recipient, thiccbody)
+		print ("BOOTY CALL SENT")
+	except:
+		print ("OH DEAR NO GIRDLE FOR YOU HERC </3")
+	finally:
+		smtp.quit()
 
-try:
-	smtp = stmplib.SMTP('smtp.gmail.com', 587)
-	smtp.starttls()
-	smtp.login(sender, senderpasswd);
-	smtp.sendmail(sender, recipient, message)
-	smtp.quit()
-	print ("Message sent")
-except:
-	print ("OH DEAR NO GIRDLE FOR YOU HERC <3")
+while True:
+	start = raw_input('Hey you wanna send an email? (y/n) ')
+	if start == 'y' or start == 'Y':
+		baby_tell_me_what_you_want()
+	elif start == 'n' or start == 'N':
+		print ('Cool bye!!')
+		quit()
