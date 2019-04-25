5223d6db30870e6fec5d156bd2a60f02a595e155
Sabrina Settle <ssettle@e1z2r8p20.42.us.org>
2019-04-24 20:04:17 -0700

    cool cat...tle

--- 09Cattle/siege.py
+++ 09Cattle/siege.py
@@ -6,7 +6,7 @@
 #    By: ssettle <ssettle@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/04/24 18:34:36 by ssettle           #+#    #+#              #
-#    Updated: 2019/04/24 19:01:10 by ssettle          ###   ########.fr        #
+#    Updated: 2019/04/24 20:03:49 by ssettle          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,12 +14,32 @@ import requests
 import sys
 import time
 import threading
+import os
 
 # ATTACK
 
 def attack():
 	print('Prepare to do battle!')
-
+	i = 0
+	hits = 0
+	while i <  :#something
+		try:
+			r = requests.get(url, timeout=3)
+			if r.status_code == 200
+				data['success'] += 1
+			else
+				data['failure'] += 1
+			data['time'] += float(str(r.elapsed)[5:11])
+			size = len(r.content)
+			date['size'] += size
+			method = str(r.request)[18:-2]
+			print 'HTTP/1.1 ', status, '\t', time, 'secs\t', size, 'bytes ==>', method, '', url
+			hits += 1
+		except:
+			data['failure'] += 1
+			print 'Bad url'
+			sys.exit(1)
+		i += 1
 # FLAGS
 
 
@@ -29,7 +49,8 @@ def attack():
 print '\nTransactions:\t\t\t', data['transactions'], 'hits'
 print 'Availability:\t\t\t', (data['success'] * 100) / data['transactions'], '%'
 print 'Elapsed time:\t\t\t', data['time'], 'secs'
-print 'Average response time:\t\t', data['time'] / data['transactions'], 'secs'
 print 'Data transfered:\t\t', data['size'] / 1024, 'KB'
+print 'Average response time:\t\t', data['time'] / data['transactions'], 'secs'
 print 'Successful transactions:\t', data['success']
 print 'Failed transactions:\t\t', data['failure']
+print 'Totaled response times:\t\t', round(data['time'], 3), 'secs'
