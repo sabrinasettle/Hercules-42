f8b93b344ddd0dea0a942974c4b67d0394611e48
Sabrina Settle <ssettle@e1z2r8p20.42.us.org>
2019-04-29 17:12:35 -0700

    siege

--- 09Cattle/siege.py
+++ 09Cattle/siege.py
@@ -6,7 +6,7 @@
 #    By: ssettle <ssettle@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/04/24 18:34:36 by ssettle           #+#    #+#              #
-#    Updated: 2019/04/29 16:21:48 by ssettle          ###   ########.fr        #
+#    Updated: 2019/04/29 17:11:19 by ssettle          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,9 +15,9 @@
 import requests
 import sys
 import time
-import threading
+import threading #havent used yet
 import os
-import argparse
+import argparse #havent used yet
 
 # PARAMETERS
 url = 'https://google.com'
@@ -52,20 +52,21 @@ while i < clients:
 					results['success'] += 1
 				else:
 					results['failure'] += 1
-				results['time'] += float(str(r.elapsed)[5:11])
+				results['time'] += float(str(r.elapsed)[5:-4]) #numeric string / sign
 				size = len(r.content)
 				results['size'] += size
 				method = str(r.request)[18:-2]
-				# print ('HTTP/1.1 ', status, '\t', time, 'secs\t', size, 'bytes ==>', method, '', url)
+				print 'HTTP/1.1 ', r.status_code, '\t', results['time'], 'secs\t', size, 'bytes ==>', method, '', url
 				hits += 1
 		i += 1
 
 # PRINTS RESULTS FROM ATTACK
-print ('\nTransactions:\t\t\t', results['transactions'], 'hits')
-print ('Availability:\t\t\t', (results['success'] * 100) / results['transactions'], '%')
-print ('Elapsed time:\t\t\t', results['time'], 'secs')
-print ('Data transfered:\t\t', results['size'] / 1024, 'KB')
-print ('Average response time:\t\t', results['time'] / results['transactions'], 'secs')
-print ('Successful transactions:\t', results['success'])
-print ('Failed transactions:\t\t', results['failure'])
-print ('Totaled response times:\t\t', round(results['time'], 3), 'secs')
+print 'And the clouds and rains parted...'
+print 'Transactions:\t\t\t', results['transactions'], 'hits'
+print 'Availability:\t\t\t', (results['success'] * 100) / results['transactions'], '%'
+print 'Elapsed time:\t\t\t', results['time'], 'secs'
+print 'Data transfered:\t\t', results['size'] / 1024, 'KB'
+print 'Average response time:\t\t', results['time'] / results['transactions'], 'secs'
+print 'Successful transactions:\t', results['success']
+print 'Failed transactions:\t\t', results['failure']
+print 'Totaled response times:\t\t', round(results['time'], 3), 'secs'
